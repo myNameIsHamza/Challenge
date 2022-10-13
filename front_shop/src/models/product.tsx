a07f33232c325b2fcc5821416e613fda7576705a
unknown <ouhammi.hamza@gmail.com>
2022-10-13 22:27:35 +0100

    add category and product models

---
+++ front_shop/src/models/product.tsx
@@ -0,0 +1,13 @@
+export default class Product {
+    id?: number;
+    name: string;
+    description: string;
+    price: Number;
+
+    constructor(id: number,name: string, description: string, price: Number) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    } 
+}
