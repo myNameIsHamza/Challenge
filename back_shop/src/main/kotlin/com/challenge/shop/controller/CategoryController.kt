a7e48c6af90a51d7b44dad50389ff3f4ea228752
unknown <ouhammi.hamza@gmail.com>
2022-10-15 16:29:20 +0100

    add category crud service and controller

--- back_shop/src/main/kotlin/com/challenge/shop/controller/CategoryController.kt
+++ back_shop/src/main/kotlin/com/challenge/shop/controller/CategoryController.kt
@@ -1,14 +1,67 @@
 package com.challenge.shop.controller
 
+import com.challenge.shop.model.Category
+import com.challenge.shop.service.CategoryService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
-class CategoryController {
+class CategoryController (@Autowired private val  categoryService:CategoryService){
 
-    @GetMapping("categories")
-    fun welcome(): ResponseEntity<String> =
-        ResponseEntity.status(HttpStatus.OK).body("Welcome to categories")
+
+    //gets all categories
+    @GetMapping("/categories")
+    fun getAllCategories() : ResponseEntity<List<Category>> =
+        ResponseEntity.status(HttpStatus.OK)
+            .body(categoryService.getAllCategories())
+
+
+    //gets the requested cqtegory
+    @GetMapping("categories/{id}")
+    fun getCategoryByid(@PathVariable id : Long) : Category =
+        categoryService.getCategoryById(id) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND,
+            "This cqtegory does not exist")
+
+
+    //creates a new cqtegory
+    @PostMapping("/categories")
+    fun saveCategories(@RequestBody cqtegory : Category) : ResponseEntity<Category>   {
+        try {
+            return ResponseEntity.status(HttpStatus.OK)
+                .body(categoryService.saveCategories(cqtegory))
+        }catch (e : Exception){
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST,
+                e.message)
+        }
+
+    }
+
+    //updates an existing cqtegory
+    @PutMapping("/categories/{id}")
+    fun updateCategory(@PathVariable id : Long, @RequestBody cqtegory : Category): ResponseEntity<Category> {
+        try{
+            return ResponseEntity.status(HttpStatus.OK)
+                .body(categoryService.updateCategory(id,cqtegory))
+
+        }catch (e : Exception){
+                throw Exception(e.message)
+        }
+
+    }
+
+    // deletes an existing cqtegory
+    @DeleteMapping("/categories/{id}")
+    fun deleteCategory(@PathVariable id : Long) : ResponseEntity<Any>{
+        try {
+            categoryService.deleteCategory(id)
+        }
+        catch (e: Exception) {
+            // can handle it by adding to logger
+        }
+        return ResponseEntity.noContent().build()
+
+    }
 }
--- back_shop/src/main/kotlin/com/challenge/shop/service/CategoryService.kt
+++ back_shop/src/main/kotlin/com/challenge/shop/service/CategoryService.kt
@@ -1,7 +1,38 @@
 package com.challenge.shop.service
 
+import com.challenge.shop.dao.CategoryDao
+import com.challenge.shop.model.Category
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 
 @Service
-class CategoryService {
+class CategoryService(@Autowired private val categoryDao: CategoryDao) {
+    fun getAllCategories(): List<Category> =
+        categoryDao.findAll()
+
+    fun getCategoryById (@PathVariable id: Long): Category? =
+        categoryDao.findById(id).get()
+
+
+    fun saveCategories(@RequestBody category: Category): Category {
+        return categoryDao.save(category)
+    }
+
+    fun updateCategory(@PathVariable id: Long, @RequestBody category: Category): Category {
+        categoryDao.findById(id).takeIf { e -> e != null }?.let {
+            return categoryDao.save(Category(id, category.name, category.description, category.category))
+        } ?: throw Exception("Category does not exist")
+
+
+    }
+
+    fun deleteCategory(@PathVariable id: Long) {
+        try {
+            categoryDao.deleteById(id)
+        } catch (e: Exception) {
+            // can handle it by adding to logger
+        }
+    }
 }
